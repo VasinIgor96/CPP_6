1abd267114ca60594b8e563eea906d3a5226a189
student <student@PC-G-04>
2023-05-21 11:18:32 +0300

    save

--- main.cpp
+++ main.cpp
@@ -8,17 +8,27 @@ int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    int array[3][3] = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+    int array[3][3];
+
+    srand(time(NULL));
+
+   
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            array[i][j] = rand() % 10 + 1; 
+        }
+    }
 
     int sum = 0;
     int minElement = INT_MAX;
     int maxElement = INT_MIN;
     int totalElements = 0;
 
-    
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 3; j++) 
+        {
+
             sum += array[i][j];
 
             if (array[i][j] < minElement) {
@@ -35,11 +45,10 @@ int main() {
 
     double average = static_cast<double>(sum) / totalElements;
 
- 
-    std::cout << "Сума всіх елементів: " << sum << std::endl;
-    std::cout << "Середнє арифметичне: " << average << std::endl;
-    std::cout << "Мінімальний елемент: " << minElement << std::endl;
-    std::cout << "Максимальний елемент: " << maxElement << std::endl;
+    cout << "Сума всіх елементів: " << sum << endl;
+    cout << "Середнє арифметичне: " << average << endl;
+    cout << "Мінімальний елемент: " << minElement << endl;
+    cout << "Максимальний елемент: " << maxElement << endl;
 
     return 0;
 }
